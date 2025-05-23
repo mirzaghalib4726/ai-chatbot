7f0c2171eefe8d101df6778b667516a1f1adbf57
Mirza Usama Ali Baig <mirza.ghalib.822@hotmail.com>
2025-05-24 04:50:13 +0500

    Sourcemap disabled in production and telementary as well

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: false, // Disable source maps in production
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Allow Google user profile images
+  },
+  // Disable telemetry
+  telemetry: {
+    disabled: true,
+  },
 };
 
 export default nextConfig;
